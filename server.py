73ac6dff55fc422732f6859f06a694c98a493c4e
chen3yym <40581631+chen3yym@users.noreply.github.com>
2023-01-14 01:14:45 -0600

    client send message

--- server.py
+++ server.py
@@ -4,3 +4,21 @@
 import socket
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+host = socket.gethostbyname()
+port = 444
+
+serversocket.bind((host, port))
+
+serversocket.listen(3)
+
+while True:
+    clientsocket, address = serversocket.accept()
+    print("Receive connection from " % str(address))
+    message = 'Thank you for connecting to the server' + '\r\n'
+    clientsocket.send(message)
+    clientsocket.close()
+
+
+
+    
