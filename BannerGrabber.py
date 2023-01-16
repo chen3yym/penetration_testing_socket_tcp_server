bd8672182471f962e55afa7479c91fbb3438708d
chen3yym <40581631+chen3yym@users.noreply.github.com>
2023-01-15 21:21:04 -0600

    create a socket

---
+++ BannerGrabber.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import socket
+
+s = socket.socket()
+
+ip = input("Please enter the IP: ")
+port = input("Please enter the port: ")
+
+s.connect((ip, port))
+
+print(s.recv(1024))
+
+
+
